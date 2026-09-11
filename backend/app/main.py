689917eabe1e020c0a6701c5dc55674b0efd9b5d
mkserrano28 <mkserrano28@gmail.com>
2026-09-12 02:01:47 +0800

    Allow www custom domain in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -121,6 +121,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://d1kcuhx3712zl4.cloudfront.net",
+        "https://www.imac-ai.site",
     ],
     allow_credentials=True,
     allow_methods=["*"],
